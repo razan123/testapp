0a778f6f5ba25450152e098a6b359dbb62729fa8
razan123 <rajan4ualwz@hotmail.com>
2014-11-11 16:36:30 +0545

    Comment Feature Installed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141110112232) do
+ActiveRecord::Schema.define(version: 20141111103859) do
+
+  create_table "comments", force: true do |t|
+    t.string   "user_name"
+    t.text     "body"
+    t.integer  "idea_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "ideas", force: true do |t|
     t.string   "name"
